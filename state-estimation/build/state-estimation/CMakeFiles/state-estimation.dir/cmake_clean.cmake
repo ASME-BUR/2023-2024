9acdeb03b7120ce19d86c8420295bc4f20abd1f0
Nakul Joshi <nakulsaliljoshi@gmail.com>
2023-12-02 13:22:12 -0800

    pre frick up

---
+++ state-estimation/build/state-estimation/CMakeFiles/state-estimation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/state-estimation.dir/src/Node.cpp.o"
+  "CMakeFiles/state-estimation.dir/src/eskf.cpp.o"
+  "state-estimation"
+  "state-estimation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/state-estimation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

a371bdb8ed5ecf6dc455e1369ad789eb56fddbaa
AlexX <ax90l0tl@gmail.com>
2024-02-13 11:24:06 -0800

    link to fork

--- state-estimation/build/state-estimation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nakul/Desktop/bur/ros2_ws/src/2023-2024/state-estimation
-# Build directory: /home/nakul/Desktop/bur/ros2_ws/src/2023-2024/state-estimation/build/state-estimation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

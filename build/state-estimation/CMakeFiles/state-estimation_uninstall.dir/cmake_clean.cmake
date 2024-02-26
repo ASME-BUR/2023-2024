88223536952df7119bd04bfbe95f05ac108fcb44
AlexX <ax90l0tl@gmail.com>
2024-02-26 15:03:37 -0800

    cleanup

--- build/state-estimation/CMakeFiles/state-estimation_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/state-estimation_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/state-estimation_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()

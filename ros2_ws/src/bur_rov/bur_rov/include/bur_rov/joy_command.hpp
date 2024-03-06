1f25caa61a8617db30d275a0f92678c584545252
Stanley Wei <stanley.wei.04@gmail.com>
2024-03-06 05:34:02 +0000

    Added pose listener + master ROV launch file

--- ros2_ws/src/bur_rov/bur_rov/include/bur_rov/joy_command.hpp
+++ ros2_ws/src/bur_rov/bur_rov/include/bur_rov/joy_command.hpp
@@ -2,6 +2,7 @@
 #define JOY_COMMAND_HPP
 
 #include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/pose.hpp"
 #include "sensor_msgs/msg/joy.hpp"
 #include "bur_rov_msgs/msg/command.hpp"
 #include <iostream>
@@ -15,8 +16,12 @@ public:
 
 private:
     void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg);
+    void pose_callback(const geometry_msgs::msg::Pose::SharedPtr msg);
     rclcpp::Publisher<bur_rov_msgs::msg::Command>::SharedPtr pub_;
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr sub_;
+    rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr poseSub;
+
+    geometry_msgs::msg::Pose pose;
 };
 
 #endif

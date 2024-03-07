61ca348db30b46c1b473c51345c5b8398000fd73
AlexX <ax90l0tl@gmail.com>
2024-03-06 20:39:14 -0800

    edit joy_command

--- ros2_ws/src/bur_rov/bur_rov/include/bur_rov/joy_command.hpp
+++ ros2_ws/src/bur_rov/bur_rov/include/bur_rov/joy_command.hpp
@@ -4,6 +4,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/pose.hpp"
 #include "sensor_msgs/msg/joy.hpp"
+#include "sensor_msgs/msg/imu.hpp"
 #include "bur_rov_msgs/msg/command.hpp"
 #include <iostream>
 #include <memory>
@@ -17,11 +18,13 @@ public:
 private:
     void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg);
     void pose_callback(const geometry_msgs::msg::Pose::SharedPtr msg);
-    rclcpp::Publisher<bur_rov_msgs::msg::Command>::SharedPtr pub_;
-    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr sub_;
-    rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr poseSub;
-
+    void imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg);
+    rclcpp::Publisher<bur_rov_msgs::msg::Command>::SharedPtr cmd_pub;
+    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub;
+    rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr pose_sub;
+    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub;
     geometry_msgs::msg::Pose pose;
+    bur_rov_msgs::msg::Command output;
 };
 
 #endif

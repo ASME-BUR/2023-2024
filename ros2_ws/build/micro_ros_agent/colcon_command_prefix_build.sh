e61a740a1491ba822f87b3cd044fb113e2a7c9b0
Alex Xu <ax90l0tl@gmail.com>
2024-03-07 09:53:29 -0800

    remove install build

--- ros2_ws/build/micro_ros_agent/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/peach/2023-2024/ros2_ws/install/micro_ros_msgs/share/micro_ros_msgs/package.sh"
--- ros2_ws/build/micro_ros_msgs/micro_ros_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/peach/2023-2024/ros2_ws/build/micro_ros_msgs/micro_ros_msgs__py
-# Build directory: /home/peach/2023-2024/ros2_ws/build/micro_ros_msgs/micro_ros_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

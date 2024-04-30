4125f9691880544eb316ac3a3bf436ef88f579e3
AlexX <ax90l0tl@gmail.com>
2024-04-30 01:35:22 -0700

    code cleanup and fixes

--- ros2_ws/src/bur_rov/bur_rov/src/joy_command.cpp
+++ ros2_ws/src/bur_rov/bur_rov/src/joy_command.cpp
@@ -1,6 +1,4 @@
 #include "joy_command.hpp"
-#include <Eigen/Dense>
-#include <Eigen/Geometry>
 #include <iostream>
 #include <cmath>
 
@@ -35,26 +33,30 @@ void JoyCommand::joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg)
 {
     if (!msg->axes.empty())
     {
-        this->output.target_vel.linear.x = vel_cap * msg->axes[axis_mapping_.at("linear_x")];
-        this->output.target_vel.linear.y = vel_cap * -msg->axes[axis_mapping_.at("linear_y")];
-        this->output.target_vel.linear.z = vel_cap * msg->axes[axis_mapping_.at("linear_z")];
-        this->output.target_vel.angular.x = vel_cap * msg->axes[axis_mapping_.at("angular_x")];
-        this->output.target_vel.angular.y = vel_cap * msg->axes[axis_mapping_.at("angular_y")];
-        this->output.target_vel.angular.z = vel_cap * msg->axes[axis_mapping_.at("angular_z")];
+        output.header.stamp = this->now();
+        output.target_vel.linear.x = vel_cap * msg->axes[axis_mapping_.at("linear_x")];
+        output.target_vel.linear.y = vel_cap * -msg->axes[axis_mapping_.at("linear_y")];
+        output.target_vel.linear.z = vel_cap * msg->axes[axis_mapping_.at("linear_z")];
+        output.target_vel.angular.x = vel_cap * msg->axes[axis_mapping_.at("angular_x")];
+        output.target_vel.angular.y = vel_cap * msg->axes[axis_mapping_.at("angular_y")];
+        output.target_vel.angular.z = vel_cap * msg->axes[axis_mapping_.at("angular_z")];
 
         tf2::Quaternion q;
         q.setRPY(msg->axes[axis_mapping_.at("angular_x")] * M_PI_2, msg->axes[axis_mapping_.at("angular_y")] * M_PI_2, 0);
         q.normalize();
-        this->output.target_pos.orientation = tf2::toMsg(q);
-        this->output.buttons.clear();
+        output.target_pos.orientation = tf2::toMsg(q);
+
+        output.buttons.clear();
         for (uint8_t i = 0; i < msg->buttons.size(); i++)
         {
-            this->output.buttons.push_back(msg->buttons[i]);
+            output.buttons.push_back(msg->buttons[i]);
         }
         cmd_pub->publish(output);
 
+        // Debug
         auto joy_euler_msg = geometry_msgs::msg::Vector3Stamped();
         joy_euler_msg.header.stamp = this->now();
+        joy_euler_msg.header.frame_id = "map";
         joy_euler_msg.vector.x = msg->axes[axis_mapping_.at("angular_x")] * M_PI_2;
         joy_euler_msg.vector.y = msg->axes[axis_mapping_.at("angular_y")] * M_PI_2;
         joy_euler->publish(joy_euler_msg);
@@ -62,47 +64,45 @@ void JoyCommand::joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg)
         auto debug_msg = geometry_msgs::msg::PoseStamped();
         debug_msg.header.stamp = this->now();
         debug_msg.header.frame_id = "map";
-
-        debug_msg.pose.orientation.w = this->output.target_pos.orientation.w;
-        debug_msg.pose.orientation.x = this->output.target_pos.orientation.x;
-        debug_msg.pose.orientation.y = this->output.target_pos.orientation.y;
-        debug_msg.pose.orientation.z = this->output.target_pos.orientation.z;
+        debug_msg.pose.orientation.w = q.getW();
+        debug_msg.pose.orientation.x = q.getX();
+        debug_msg.pose.orientation.y = q.getY();
+        debug_msg.pose.orientation.z = q.getZ();
 
         debug_msg.pose.position.x = 0;
         debug_msg.pose.position.y = 0;
         debug_msg.pose.position.z = 0;
-
         debug_joy_pub->publish(debug_msg);
     }
 }
 
 void JoyCommand::pose_callback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg)
 {
-    this->output.current_pos.position.z = msg->pose.pose.position.z;
+    output.header.stamp = this->now();
+    output.current_pos.position.z = msg->pose.pose.position.z;
     cmd_pub->publish(output);
 }
 
 void JoyCommand::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
 {
-    this->output.current_pos.orientation.x = msg->orientation.x;
-    this->output.current_pos.orientation.y = msg->orientation.y;
-    this->output.current_pos.orientation.z = msg->orientation.z;
-    this->output.current_pos.orientation.w = msg->orientation.w;
+    output.header.stamp = this->now();
+    output.header.frame_id = "map";
+    output.current_pos.orientation.x = msg->orientation.x;
+    output.current_pos.orientation.y = msg->orientation.y;
+    output.current_pos.orientation.z = msg->orientation.z;
+    output.current_pos.orientation.w = msg->orientation.w;
 
-    this->output.current_vel.angular.x = msg->angular_velocity.x;
-    this->output.current_vel.angular.y = msg->angular_velocity.y;
-    this->output.current_vel.angular.z = msg->angular_velocity.z;
+    output.current_vel.angular.x = msg->angular_velocity.x;
+    output.current_vel.angular.y = msg->angular_velocity.y;
+    output.current_vel.angular.z = msg->angular_velocity.z;
     rclcpp::Time now = this->now();
     double acceleration[3] = {msg->linear_acceleration.x, msg->linear_acceleration.y, -msg->linear_acceleration.z};
 
     // Gravity adjustment
-    Eigen::Quaterniond orientation(msg->orientation.w,
-                                   msg->orientation.x,
-                                   msg->orientation.y,
-                                   msg->orientation.z);
-
-    Eigen::Vector3d gravity = {0, 0, 9.80665};
-    Eigen::Vector3d gravityOffset = orientation * gravity;
+    tf2::Vector3 gravity = tf2::Vector3(0, 0, 9.80665);
+    tf2::Quaternion q = tf2::Quaternion(msg->orientation.w, msg->orientation.x, msg->orientation.y, msg->orientation.z);
+    tf2::Matrix3x3 rot_matrix = tf2::Matrix3x3(q);
+    tf2::Vector3 gravityOffset = tf2::Vector3(rot_matrix*gravity);
 
     acceleration[0] += gravityOffset[0];
     acceleration[1] += gravityOffset[1];
@@ -110,9 +110,9 @@ void JoyCommand::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
 
     vel_calc(acceleration, now, prev_time);
 
-    this->output.current_vel.linear.x = velocity[0];
-    this->output.current_vel.linear.y = velocity[1];
-    this->output.current_vel.linear.z = velocity[2];
+    // output.current_vel.linear.x = velocity[0];
+    // output.current_vel.linear.y = velocity[1];
+    // output.current_vel.linear.z = velocity[2];
 
     cmd_pub->publish(output);
     prev_time = this->now();
@@ -120,12 +120,10 @@ void JoyCommand::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
     auto debug_msg = geometry_msgs::msg::PoseStamped();
     debug_msg.header.stamp = this->now();
     debug_msg.header.frame_id = "map";
-
-    debug_msg.pose.orientation.w = this->output.current_pos.orientation.w;
-    debug_msg.pose.orientation.x = this->output.current_pos.orientation.x;
-    debug_msg.pose.orientation.y = this->output.current_pos.orientation.y;
-    debug_msg.pose.orientation.z = this->output.current_pos.orientation.z;
-
+    debug_msg.pose.orientation.w = output.current_pos.orientation.w;
+    debug_msg.pose.orientation.x = output.current_pos.orientation.x;
+    debug_msg.pose.orientation.y = output.current_pos.orientation.y;
+    debug_msg.pose.orientation.z = output.current_pos.orientation.z;
     debug_msg.pose.position.x = 0;
     debug_msg.pose.position.y = 0;
     debug_msg.pose.position.z = 0;
@@ -134,20 +132,10 @@ void JoyCommand::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
 
     auto imu_euler_msg = geometry_msgs::msg::Vector3Stamped();
     imu_euler_msg.header.stamp = this->now();
-    // roll (x-axis rotation)
-    double sinr_cosp = 2 * (this->output.current_pos.orientation.w * this->output.current_pos.orientation.x + this->output.current_pos.orientation.y * this->output.current_pos.orientation.z);
-    double cosr_cosp = 1 - 2 * (this->output.current_pos.orientation.x * this->output.current_pos.orientation.x + this->output.current_pos.orientation.y * this->output.current_pos.orientation.y);
-    double roll = std::atan2(sinr_cosp, cosr_cosp);
+    imu_euler_msg.header.frame_id = "map";
 
-    // pitch (y-axis rotation)
-    double sinp = std::sqrt(1 + 2 * (this->output.current_pos.orientation.w * this->output.current_pos.orientation.y - this->output.current_pos.orientation.x * this->output.current_pos.orientation.z));
-    double cosp = std::sqrt(1 - 2 * (this->output.current_pos.orientation.w * this->output.current_pos.orientation.y - this->output.current_pos.orientation.x * this->output.current_pos.orientation.z));
-    double pitch = 2 * std::atan2(sinp, cosp) - M_PI / 2;
-
-    // yaw (z-axis rotation)
-    double siny_cosp = 2 * (this->output.current_pos.orientation.w * this->output.current_pos.orientation.z + this->output.current_pos.orientation.x * this->output.current_pos.orientation.y);
-    double cosy_cosp = 1 - 2 * (this->output.current_pos.orientation.y * this->output.current_pos.orientation.y + this->output.current_pos.orientation.z * this->output.current_pos.orientation.z);
-    double yaw = std::atan2(siny_cosp, cosy_cosp);
+    double roll, pitch, yaw;
+    rot_matrix.getRPY(roll, pitch, yaw);
 
     imu_euler_msg.vector.x = roll;
     imu_euler_msg.vector.y = pitch;

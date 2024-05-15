d536c45a4d935dc3a8075a9adbaac679e1c364b5
BUR <bur.asmebruins@gmail.com>
2024-05-14 20:46:16 -0700

    verbose output launch arg

--- ros2_ws/src/bur_rov/bur_rov/launch/micro_ros.launch.py
+++ ros2_ws/src/bur_rov/bur_rov/launch/micro_ros.launch.py
@@ -1,21 +1,40 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, TimerAction
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PythonExpression
 
 
 def generate_launch_description():
-    return LaunchDescription(
-        [
-            Node(
-                package="micro_ros_agent",
-                executable="micro_ros_agent",
-                output='screen',
-                arguments=["serial", "-D", "/dev/ttyACM1", "-v4", "-b", "250000"]
-            ),
-            Node(
-                package="micro_ros_agent",
-                executable="micro_ros_agent",
-                output='screen',
-                arguments=["serial", "-D", "/dev/ttyACM0", "-v4"]
-            )
-        ]
+    imu_verbose = LaunchConfiguration('imu_verbose')
+    thruster_verbose = LaunchConfiguration('thruster_verbose')
+    imu_verbose_arg = DeclareLaunchArgument(
+        'imu_verbose',
+        default_value='4'
     )
+    thruster_verbose_arg = DeclareLaunchArgument(
+        'thruster_verbose',
+        default_value='4'
+    )
+
+
+    imu = Node(
+                package="micro_ros_agent",
+                executable="micro_ros_agent",
+                output='screen',
+                arguments=["serial", "-D", "/dev/ttyACM1", "-v", imu_verbose, "-b", "250000"]
+            )
+    thruster = Node(
+                package="micro_ros_agent",
+                executable="micro_ros_agent",
+                output='screen',
+                arguments=["serial", "-D", "/dev/ttyACM0", "-v", thruster_verbose]
+            )
+    
+    return LaunchDescription([
+        imu_verbose_arg,
+        thruster_verbose_arg,
+        imu,
+        thruster,
+    ])

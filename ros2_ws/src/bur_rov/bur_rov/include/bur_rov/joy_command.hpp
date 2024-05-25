c87cd0f2db388212bba59c4d565d1fd8f960a1a2
AlexX <ax90l0tl@gmail.com>
2024-05-25 13:18:01 -0700

    set pose and twist

--- ros2_ws/src/bur_rov/bur_rov/include/bur_rov/joy_command.hpp
+++ ros2_ws/src/bur_rov/bur_rov/include/bur_rov/joy_command.hpp
@@ -4,6 +4,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
 #include "geometry_msgs/msg/vector3_stamped.hpp"
+#include "geometry_msgs/msg/twist_stamped.hpp"
 #include "sensor_msgs/msg/joy.hpp"
 #include "sensor_msgs/msg/imu.hpp"
 #include "nav_msgs/msg/odometry.hpp"
@@ -11,6 +12,7 @@
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/utils.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include "rcl_interfaces/msg/set_parameters_result.hpp"
 #include <iostream>
 #include <map>
 #include <vector>
@@ -26,9 +28,14 @@ public:
 
 private:
     void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg);
-    void pose_callback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg);
+    void depth_callback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg);
+    void pose_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg);
+    void twist_callback(const geometry_msgs::msg::TwistStamped::SharedPtr msg);
     void imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg);
     void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
+    void timer_callback();
+    void create_subscribers();
+    void set_constants();
     double *vel_calc(double acceleration[3], rclcpp::Time current_time, rclcpp::Time previous_time);
 
     rclcpp::Publisher<bur_rov_msgs::msg::Command>::SharedPtr cmd_pub;
@@ -38,12 +45,21 @@ private:
     rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr debug_joy_pub;
 
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub;
-    rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr pose_sub;
+    rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr twist_sub;
+    rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr depth_sub;
+    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_sub;
     rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub;
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub;
-    
+    rclcpp::TimerBase::SharedPtr timer;
+
+    rcl_interfaces::msg::SetParametersResult parametersCallback(
+        const std::vector<rclcpp::Parameter> &parameters);
+    rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr param_callback;
+
     bur_rov_msgs::msg::Command output;
     geometry_msgs::msg::Vector3Stamped joy_euler_msg;
+    bool using_joy;
+    bool using_ekf;
     float multiplier;
     double v_0[3] = {};
     double velocity[3] = {};
--- ros2_ws/src/bur_rov/bur_rov/src/joy_command.cpp
+++ ros2_ws/src/bur_rov/bur_rov/src/joy_command.cpp
@@ -5,139 +5,185 @@
 JoyCommand::JoyCommand() : rclcpp::Node("joy_command")
 {
     this->declare_parameter("cmd_pub_topic", "command");
+    this->declare_parameter("twist_topic", "twist");
     this->declare_parameter("joy_topic", "joy");
-    this->declare_parameter("pose_topic", "depth_sensor");
+    this->declare_parameter("depth_topic", "depth_sensor");
+    this->declare_parameter("pose_topic", "set_pose");
     this->declare_parameter("imu_topic", "imu");
     this->declare_parameter("multiplier", 0.5);
     const std::map<std::string, int> &axis_mapping = {{"linear_x", 1}, {"linear_y", 0}, {"linear_z", 2}, {"angular_x", 3}, {"angular_y", 4}, {"angular_z", 5}};
     this->declare_parameters("axis_mapping", axis_mapping);
     this->declare_parameter("using_ekf", false);
     this->declare_parameter("using_joy", true);
+    this->declare_parameter("publish_rate", 100);
     cmd_pub = this->create_publisher<bur_rov_msgs::msg::Command>(this->get_parameter("cmd_pub_topic").as_string(), 10);
-    if (this->get_parameter("using_joy").as_bool())
-    {
-        joy_sub = this->create_subscription<sensor_msgs::msg::Joy>(this->get_parameter("joy_topic").as_string(), 10, bind(&JoyCommand::joy_callback, this, placeholders::_1));
-    }
-    if (this->get_parameter("using_ekf").as_bool())
-    {
-        odom_sub = this->create_subscription<nav_msgs::msg::Odometry>("/odometry/filtered", 10, bind(&JoyCommand::odom_callback, this, placeholders::_1));
-    }
-    else
-    {
-        pose_sub = this->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(this->get_parameter("pose_topic").as_string(), 10, bind(&JoyCommand::pose_callback, this, placeholders::_1));
-    }
-    const rclcpp::QoS qos_profile = rclcpp::QoS(rclcpp::KeepLast(10)).best_effort().durability_volatile();
-    imu_sub = this->create_subscription<sensor_msgs::msg::Imu>(this->get_parameter("imu_topic").as_string(), qos_profile,
-                                                               bind(&JoyCommand::imu_callback, this, placeholders::_1));
-    multiplier = this->get_parameter("multiplier").as_double();
-    this->get_parameters("axis_mapping", axis_mapping_);
 
+    int publish_rate = this->get_parameter("publish_rate").as_int();
+    timer = this->create_wall_timer(std::chrono::milliseconds(1000 / publish_rate), std::bind(&JoyCommand::timer_callback, this));
+    create_subscribers();
+    set_constants();
     prev_time = this->now();
-
     debug_imu_pub = this->create_publisher<geometry_msgs::msg::PoseStamped>("imu_debug", 10);
     debug_joy_pub = this->create_publisher<geometry_msgs::msg::PoseStamped>("joy_debug", 10);
     imu_euler = this->create_publisher<geometry_msgs::msg::Vector3Stamped>("imu_euler", 10);
     joy_euler = this->create_publisher<geometry_msgs::msg::Vector3Stamped>("joy_euler", 10);
 }
 
+void JoyCommand::create_subscribers()
+{
+    param_callback = this->add_on_set_parameters_callback(bind(&JoyCommand::parametersCallback, this, placeholders::_1));
+    joy_sub = this->create_subscription<sensor_msgs::msg::Joy>(this->get_parameter("joy_topic").as_string(), 10, bind(&JoyCommand::joy_callback, this, placeholders::_1));
+    twist_sub = this->create_subscription<geometry_msgs::msg::TwistStamped>(this->get_parameter("twist_topic").as_string(), 10, bind(&JoyCommand::twist_callback, this, placeholders::_1));
+    odom_sub = this->create_subscription<nav_msgs::msg::Odometry>("/odometry/filtered", 10, bind(&JoyCommand::odom_callback, this, placeholders::_1));
+    pose_sub = this->create_subscription<geometry_msgs::msg::PoseStamped>(this->get_parameter("pose_topic").as_string(), 10, bind(&JoyCommand::pose_callback, this, placeholders::_1));
+    depth_sub = this->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(this->get_parameter("depth_topic").as_string(), 10, bind(&JoyCommand::depth_callback, this, placeholders::_1));
+    const rclcpp::QoS qos_profile = rclcpp::QoS(rclcpp::KeepLast(10)).best_effort().durability_volatile();
+    imu_sub = this->create_subscription<sensor_msgs::msg::Imu>(this->get_parameter("imu_topic").as_string(), qos_profile, bind(&JoyCommand::imu_callback, this, placeholders::_1));
+}
+void JoyCommand::set_constants()
+{
+    using_joy = this->get_parameter("using_joy").as_bool();
+    using_ekf = this->get_parameter("using_ekf").as_bool();
+    multiplier = this->get_parameter("multiplier").as_double();
+    this->get_parameters("axis_mapping", axis_mapping_);
+}
+rcl_interfaces::msg::SetParametersResult JoyCommand::parametersCallback(
+    const vector<rclcpp::Parameter> &parameters)
+{
+    rcl_interfaces::msg::SetParametersResult result;
+    result.successful = true;
+    result.reason = "success";
+    for (const auto &param : parameters)
+    {
+        RCLCPP_INFO(this->get_logger(), "%s", param.get_name().c_str());
+        RCLCPP_INFO(this->get_logger(), "%s", param.get_type_name().c_str());
+        RCLCPP_INFO(this->get_logger(), "%s", param.value_to_string().c_str());
+    }
+    set_constants();
+    return result;
+}
+
 void JoyCommand::joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg)
 {
-    if (!msg->axes.empty())
+    if (using_joy)
     {
-        output.header.stamp = this->now();
-        output.header.frame_id = "odom";
-        output.target_vel.header.stamp = this->now();
-        output.target_vel.header.frame_id = "odom";
-        output.target_vel.twist.linear.x = multiplier * -msg->axes[abs(axis_mapping_.at("linear_x"))];
-        output.target_vel.twist.linear.y = multiplier * msg->axes[abs(axis_mapping_.at("linear_y"))];
-        output.target_vel.twist.linear.z = multiplier * msg->axes[abs(axis_mapping_.at("linear_z"))];
-        output.target_vel.twist.angular.x = multiplier * msg->axes[abs(axis_mapping_.at("angular_x"))];
-        output.target_vel.twist.angular.y = multiplier * msg->axes[abs(axis_mapping_.at("angular_y"))];
-        output.target_vel.twist.angular.z = multiplier * msg->axes[abs(axis_mapping_.at("angular_z"))];
 
-        // Debug
-        joy_euler_msg.header.stamp = this->now();
-        joy_euler_msg.header.frame_id = "odom";
-        if (msg->buttons[8])
+        if (!msg->axes.empty())
         {
-            joy_euler_msg.vector.x = 0;
-            joy_euler_msg.vector.y = 0;
+            output.header.stamp = this->now();
+            output.header.frame_id = "odom";
+            output.target_vel.header.stamp = this->now();
+            output.target_vel.header.frame_id = "odom";
+            output.target_vel.twist.linear.x = multiplier * -msg->axes[abs(axis_mapping_.at("linear_x"))];
+            output.target_vel.twist.linear.y = multiplier * msg->axes[abs(axis_mapping_.at("linear_y"))];
+            output.target_vel.twist.linear.z = multiplier * msg->axes[abs(axis_mapping_.at("linear_z"))];
+            output.target_vel.twist.angular.x = multiplier * msg->axes[abs(axis_mapping_.at("angular_x"))];
+            output.target_vel.twist.angular.y = multiplier * msg->axes[abs(axis_mapping_.at("angular_y"))];
+            output.target_vel.twist.angular.z = multiplier * msg->axes[abs(axis_mapping_.at("angular_z"))];
+
+            // Debug
+            joy_euler_msg.header.stamp = this->now();
+            joy_euler_msg.header.frame_id = "odom";
+            if (msg->buttons[8])
+            {
+                joy_euler_msg.vector.x = 0;
+                joy_euler_msg.vector.y = 0;
+            }
+            else
+            {
+                joy_euler_msg.vector.x += msg->axes[axis_mapping_.at("angular_x")];
+                joy_euler_msg.vector.y += msg->axes[axis_mapping_.at("angular_y")];
+            }
+            joy_euler->publish(joy_euler_msg);
+
+            tf2::Quaternion q;
+            q.setRPY(joy_euler_msg.vector.x * M_PI_2 / 180, joy_euler_msg.vector.y * M_PI_2 / 180, 0);
+            q.normalize();
+            output.target_pos.pose.orientation = tf2::toMsg(q);
+
+            output.buttons.clear();
+            for (uint8_t i = 0; i < msg->buttons.size(); i++)
+            {
+                output.buttons.push_back(msg->buttons[i]);
+            }
+            cmd_pub->publish(output);
+
+            auto debug_msg = geometry_msgs::msg::PoseStamped();
+            debug_msg.header.stamp = this->now();
+            debug_msg.header.frame_id = "odom";
+            debug_msg.pose.orientation.w = q.getW();
+            debug_msg.pose.orientation.x = q.getX();
+            debug_msg.pose.orientation.y = q.getY();
+            debug_msg.pose.orientation.z = q.getZ();
+
+            debug_msg.pose.position.x = 0;
+            debug_msg.pose.position.y = 0;
+            debug_msg.pose.position.z = 0;
+            debug_joy_pub->publish(debug_msg);
         }
-        else
-        {
-            joy_euler_msg.vector.x += msg->axes[axis_mapping_.at("angular_x")];
-            joy_euler_msg.vector.y += msg->axes[axis_mapping_.at("angular_y")];
-        }
-        joy_euler->publish(joy_euler_msg);
-
-        tf2::Quaternion q;
-        q.setRPY(joy_euler_msg.vector.x * M_PI_2/180, joy_euler_msg.vector.y * M_PI_2/180, 0);
-        q.normalize();
-        output.target_pos.pose.orientation = tf2::toMsg(q);
-
-        output.buttons.clear();
-        for (uint8_t i = 0; i < msg->buttons.size(); i++)
-        {
-            output.buttons.push_back(msg->buttons[i]);
-        }
-        cmd_pub->publish(output);
-
-        auto debug_msg = geometry_msgs::msg::PoseStamped();
-        debug_msg.header.stamp = this->now();
-        debug_msg.header.frame_id = "odom";
-        debug_msg.pose.orientation.w = q.getW();
-        debug_msg.pose.orientation.x = q.getX();
-        debug_msg.pose.orientation.y = q.getY();
-        debug_msg.pose.orientation.z = q.getZ();
-
-        debug_msg.pose.position.x = 0;
-        debug_msg.pose.position.y = 0;
-        debug_msg.pose.position.z = 0;
-        debug_joy_pub->publish(debug_msg);
     }
 }
 
-void JoyCommand::pose_callback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg)
+void JoyCommand::depth_callback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg)
 {
-    output.current_pos.header.stamp = this->now();
-    output.current_pos.pose.position.z = msg->pose.pose.position.z;
-    cmd_pub->publish(output);
+    if (!using_ekf)
+    {
+        output.current_pos.header.stamp = this->now();
+        output.current_pos.pose.position.z = msg->pose.pose.position.z;
+    }
+}
+
+void JoyCommand::twist_callback(const geometry_msgs::msg::TwistStamped::SharedPtr msg)
+{
+    if (!using_joy)
+    {
+        output.target_vel = *msg;
+    }
+}
+
+void JoyCommand::pose_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg)
+{
+    if (using_ekf)
+    {
+        output.target_pos = *msg;
+    }
 }
 
 void JoyCommand::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
 {
-    output.header.stamp = this->now();
-    output.header.frame_id = "odom";
+    output.current_pos.header.stamp = this->now();
+    output.current_pos.header.frame_id = "imu";
     output.current_pos.header = msg->header;
     output.current_pos.pose.orientation.x = msg->orientation.x;
     output.current_pos.pose.orientation.y = msg->orientation.y;
     output.current_pos.pose.orientation.z = msg->orientation.z;
     output.current_pos.pose.orientation.w = msg->orientation.w;
 
+    output.current_vel.header.frame_id = "imu";
     output.current_vel.twist.angular.x = msg->angular_velocity.x;
     output.current_vel.twist.angular.y = msg->angular_velocity.y;
     output.current_vel.twist.angular.z = msg->angular_velocity.z;
-    rclcpp::Time now = this->now();
-    double acceleration[3] = {msg->linear_acceleration.x, msg->linear_acceleration.y, -msg->linear_acceleration.z};
+
+    // OLD VELOCITY CALC
+    // rclcpp::Time now = this->now();
+    // double acceleration[3] = {msg->linear_acceleration.x, msg->linear_acceleration.y, -msg->linear_acceleration.z};
 
     // Gravity adjustment
-    tf2::Vector3 gravity = tf2::Vector3(0, 0, 9.80665);
+    // tf2::Vector3 gravity = tf2::Vector3(0, 0, 9.80665);
     tf2::Quaternion q = tf2::Quaternion(msg->orientation.w, msg->orientation.x, msg->orientation.y, msg->orientation.z);
     tf2::Matrix3x3 rot_matrix = tf2::Matrix3x3(q);
-    tf2::Vector3 gravityOffset = tf2::Vector3(rot_matrix * gravity);
+    // tf2::Vector3 gravityOffset = tf2::Vector3(rot_matrix * gravity);
 
-    acceleration[0] += gravityOffset[0];
-    acceleration[1] += gravityOffset[1];
-    acceleration[2] += gravityOffset[2];
+    // acceleration[0] += gravityOffset[0];
+    // acceleration[1] += gravityOffset[1];
+    // acceleration[2] += gravityOffset[2];
 
-    vel_calc(acceleration, now, prev_time);
+    // vel_calc(acceleration, now, prev_time);
 
     // output.current_vel.linear.x = velocity[0];
     // output.current_vel.linear.y = velocity[1];
     // output.current_vel.linear.z = velocity[2];
-
-    cmd_pub->publish(output);
-    prev_time = this->now();
+    // prev_time = this->now();
 
     auto debug_msg = geometry_msgs::msg::PoseStamped();
     debug_msg.header.stamp = this->now();
@@ -149,16 +195,13 @@ void JoyCommand::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
     debug_msg.pose.position.x = 0;
     debug_msg.pose.position.y = 0;
     debug_msg.pose.position.z = 0;
-
     debug_imu_pub->publish(debug_msg);
 
     auto imu_euler_msg = geometry_msgs::msg::Vector3Stamped();
     imu_euler_msg.header.stamp = this->now();
     imu_euler_msg.header.frame_id = "odom";
-
     double roll, pitch, yaw;
     rot_matrix.getRPY(roll, pitch, yaw);
-
     imu_euler_msg.vector.x = roll;
     imu_euler_msg.vector.y = pitch;
     imu_euler_msg.vector.z = yaw;
@@ -167,12 +210,19 @@ void JoyCommand::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
 
 void JoyCommand::odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
 {
-    output.header.stamp = this->now();
-    output.header.frame_id = "odom";
-    output.current_pos.header = msg->header;
-    output.current_pos.pose.position = msg->pose.pose.position;
-    output.current_vel.header = msg->header;
-    output.current_vel.twist = msg->twist.twist;
+    if (using_ekf)
+    {
+        output.header.stamp = this->now();
+        output.header.frame_id = "odom";
+        output.current_pos.header = msg->header;
+        output.current_pos.pose.position = msg->pose.pose.position;
+        output.current_vel.header = msg->header;
+        output.current_vel.twist = msg->twist.twist;
+    }
+}
+
+void JoyCommand::timer_callback()
+{
     cmd_pub->publish(output);
 }
 

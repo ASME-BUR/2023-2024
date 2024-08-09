15dee4fff2f647de5f15c0fa6bf5debd452f2938
Stanley Wei <stanley.wei.04@gmail.com>
2024-08-08 19:59:19 -0700

    Fleshed out comp behavior tree

--- ros2_ws/src/bur_auv/simple_manager/include/bt_nodes.h
+++ ros2_ws/src/bur_auv/simple_manager/include/bt_nodes.h
@@ -8,6 +8,70 @@
 
 #include "manager_node.h"
 
+class DriveAtDetected : public BT::StatefulActionNode
+{
+    public:
+        DriveAtDetected(const std::string& name, const BT::NodeConfiguration& config,
+                        const std::shared_ptr<SimpleManager> ptr,
+                        const int object_label):
+            BT::StatefulActionNode(name, config),
+            node_(ptr),
+            object_label_(object_label) {}
+        
+        static BT::PortsList providedPorts() { return {}; }
+
+        BT::NodeStatus onStart() override   { return this->publish_joy(); }
+        BT::NodeStatus onRunning() override { return this->publish_joy(); }
+
+        void onHalted() override {}
+    
+    private: 
+        std::shared_ptr<SimpleManager> node_;
+        int object_label_;
+
+        std::chrono::steady_clock::time_point timer_;
+        bool not_seen_ = false;
+
+        BT::NodeStatus publish_joy();
+
+        sensor_msgs::msg::Joy msg;
+
+};
+
+
+class DriveForDuration : public BT::StatefulActionNode
+{
+    public:
+        DriveForDuration(const std::string& name, const BT::NodeConfiguration& config,
+                        const std::shared_ptr<SimpleManager> ptr,
+                        const sensor_msgs::msg::Joy joy_msg,
+                        const float duration):
+            BT::StatefulActionNode(name, config),
+            node_(ptr),
+            joy_msg_(joy_msg),
+            duration_(duration) {
+                    begin_ = std::chrono::steady_clock::now();
+            }
+
+        static BT::PortsList providedPorts() { return {}; }
+
+        BT::NodeStatus onStart() override   { return this->publish_joy(); }
+        BT::NodeStatus onRunning() override { return this->publish_joy(); }
+
+        void onHalted() override {}
+
+    
+    private:
+        std::shared_ptr<SimpleManager> node_;
+        float duration_;
+
+        std::chrono::steady_clock::time_point begin_;
+
+        sensor_msgs::msg::Joy joy_msg_;
+        BT::NodeStatus publish_joy();
+};
+
+
 class FireTorpedo : public BT::SyncActionNode
 {
     public:
--- ros2_ws/src/bur_auv/simple_manager/include/manager_node.h
+++ ros2_ws/src/bur_auv/simple_manager/include/manager_node.h
@@ -10,12 +10,20 @@
 #include "geometry_msgs/msg/twist.hpp"
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "nav_msgs/msg/odometry.hpp"
+#include "sensor_msgs/msg/joy.hpp"
+#include "yolo_msgs/msg/cv_detection.hpp"
+#include "yolo_msgs/msg/cv_detections.hpp"
 
 #include "tf2_ros/transform_listener.h"
 #include "tf2_ros/buffer.h"
 
 #include "rclcpp/rclcpp.hpp"
 
+const int YOLO_GATE = 0;
+const int YOLO_BUOY = 1;
+
+const int ZED_WIDTH = 1920;
+const int ZED_HEIGHT = 1080;
 
 class SimpleManager : public rclcpp::Node
 {
@@ -28,21 +36,27 @@ class SimpleManager : public rclcpp::Node
         geometry_msgs::msg::Pose const getCurrentPosition() { return current_pos_; }
 
         void set_goal_pose(geometry_msgs::msg::Pose target_pos);
+        void publish_joy_msg(sensor_msgs::msg::Joy joy_msg);
 
         // Target Locations
         std::shared_ptr<geometry_msgs::msg::Pose> start_position_;
         std::shared_ptr<geometry_msgs::msg::Pose> gate_position_;
         std::vector<geometry_msgs::msg::Pose> marker_poses_;
 
+        bool buoy_is_left_ = true;
+        bool gate_complete = false;
+
+        std::vector<yolo_msgs::msg::CVDetection> detected_;
 
     private:
     
         BT::Tree behavior_tree_;
 
         rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr localizer_sub_;
-        rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr vision_sub_;
+        rclcpp::Subscription<yolo_msgs::msg::CVDetections>::SharedPtr vision_sub_;
         
         rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr goal_pose_pub_;
+        rclcpp::Publisher<sensor_msgs::msg::Joy>::SharedPtr joy_pub_;
 
         geometry_msgs::msg::Pose current_pos_;
         geometry_msgs::msg::Twist current_vel_;
@@ -62,7 +76,7 @@ class SimpleManager : public rclcpp::Node
         // ROS Callbacks
         void publish_goal_pose();
         void localizer_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
-
+        void vision_callback(const yolo_msgs::msg::CVDetections::SharedPtr msg);
 };
 
 #endif

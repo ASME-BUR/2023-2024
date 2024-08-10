40a5a49d55ea2ceff260fb1cc5c9b08ff7b8859b
Stanley Wei <stanley.wei.04@gmail.com>
2024-08-10 02:31:29 -0700

    Non-vision behavior tree

--- ros2_ws/src/bur_auv/simple_manager/include/bt_nodes.h
+++ ros2_ws/src/bur_auv/simple_manager/include/bt_nodes.h
@@ -6,72 +6,13 @@
 
 #include "behaviortree_cpp/action_node.h"
 #include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/quaternion.hpp"
+
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+
 
 #include "manager_node.h"
-
-
-class TurnTowardsBuoy : public BT::StatefulActionNode
-{
-    public:
-        TurnTowardsBuoy(const std::string& name, const BT::NodeConfiguration& config,
-                        const std::shared_ptr<SimpleManager> ptr):
-            BT::StatefulActionNode(name, config),
-            node_(ptr) {
-            this->node_->gate_complete = true;
-            begin_ = std::chrono::steady_clock::now();
-        }
-        
-        static BT::PortsList providedPorts() { return {}; }
-
-        BT::NodeStatus onStart() override   { return this->turn(); }
-        BT::NodeStatus onRunning() override { return this->turn(); }
-
-        void onHalted() override {}
-    
-    private: 
-        std::shared_ptr<SimpleManager> node_;
-
-        std::chrono::steady_clock::time_point begin_;
-        float time_limit_ = 10.0;
-
-        BT::NodeStatus turn();
-        nav_msgs::msg::Odometry odometry_msg_;
-
-};
-
-class DriveAtDetected : public BT::StatefulActionNode
-{
-    public:
-        DriveAtDetected(const std::string& name, const BT::NodeConfiguration& config,
-                        const std::shared_ptr<SimpleManager> ptr,
-                        const int object_label):
-            BT::StatefulActionNode(name, config),
-            node_(ptr),
-            object_label_(object_label) {}
-        
-        static BT::PortsList providedPorts() { return {}; }
-
-        BT::NodeStatus onStart() override   { return this->publish_joy(); }
-        BT::NodeStatus onRunning() override { return this->publish_joy(); }
-
-        void onHalted() override {}
-    
-    private: 
-        std::shared_ptr<SimpleManager> node_;
-        int object_label_;
-
-        std::chrono::steady_clock::time_point timer_;
-        bool not_seen_ = false;
-
-        float target_distance_ = -1.0;
-        float stop_distance_ = 3.0;
-
-        BT::NodeStatus publish_joy();
-
-        sensor_msgs::msg::Joy joy_msg_;
-        nav_msgs::msg::Odometry odometry_msg_;
-
-};
 
 
 class DriveForDuration : public BT::StatefulActionNode
@@ -124,6 +65,27 @@ class FireTorpedo : public BT::SyncActionNode
         std::string pub_topic_;
 };
 
+class SaveCurrentPoseToBlackboard : public BT::SyncActionNode
+{
+    public:
+        SaveCurrentPoseToBlackboard(const std::string& name, const BT::NodeConfiguration& config,
+                            const std::shared_ptr<SimpleManager> ptr):
+            BT::SyncActionNode(name, config),
+            node_(ptr) {}
+        
+        static BT::PortsList providedPorts() {
+            return { BT::OutputPort<geometry_msgs::msg::Pose>("output") };
+        }
+
+        BT::NodeStatus tick() override {
+            setOutput("output", this->node_->getCurrentPosition());
+            return BT::NodeStatus::SUCCESS;
+        }
+    
+    private:
+        std::shared_ptr<SimpleManager> node_;
+};
+
 class TurnToOrientation : public BT::StatefulActionNode
 {
     public:
@@ -141,11 +103,139 @@ class TurnToOrientation : public BT::StatefulActionNode
 
         void onHalted() override {}
 
+        std::shared_ptr<SimpleManager> getNode() { return this->node_; }
+
+        void setTarget(const tf2::Quaternion& q) {
+            this->target_ = tf2::toMsg(q);
+        }
+
+        BT::NodeStatus turn();
+
     private:
         std::shared_ptr<SimpleManager> node_;
         geometry_msgs::msg::Quaternion target_;
+};
+
+class TurnFromCurrentPosition : public TurnToOrientation
+{
+    public:
+        TurnFromCurrentPosition(const std::string& name, const BT::NodeConfiguration& config,
+                            const std::shared_ptr<SimpleManager> ptr,
+                            const tf2::Quaternion q):
+            TurnToOrientation(name,config, ptr, geometry_msgs::msg::Quaternion()),
+            transf_(q) {}
+        
+        BT::NodeStatus onStart() override {
+            auto current_pos = this->getNode()->getCurrentPosition();
+            tf2::Quaternion q = tf2::Quaternion(current_pos.orientation.x, 
+                                                current_pos.orientation.y, 
+                                                current_pos.orientation.z, 
+                                                current_pos.orientation.w);
+
+            tf2::Quaternion q_new = this->transf_ * q;
+            q_new.normalize();
+
+            this->setTarget(q_new);
+
+            return TurnToOrientation::onStart();
+        }
+
+    private:
+        tf2::Quaternion transf_;
+};
+
+class TurnToBlackboardOrientation : public TurnToOrientation
+{
+    public:
+        TurnToBlackboardOrientation(const std::string& name, const BT::NodeConfiguration& config,
+                            const std::shared_ptr<SimpleManager> ptr):
+            TurnToOrientation(name, config, ptr, geometry_msgs::msg::Quaternion()) {}
+        
+        static BT::PortsList providedPorts() {
+            return { BT::InputPort<geometry_msgs::msg::Pose>("orientation") };
+        }
+
+        BT::NodeStatus turn() {
+            auto q_msg = getInput<geometry_msgs::msg::Pose>("orientation");
+
+            if(!q_msg) {
+                throw BT::RuntimeError("missing required input [orientation]: ", 
+                              q_msg.error() );
+            }
+
+            tf2::Quaternion q;
+            tf2::fromMsg(q_msg.value().orientation, q);
+
+            this->setTarget(q);
+
+            return TurnToOrientation::turn(); 
+        }
+};
+
+
+// Vision Nodes (unused)
+
+class DriveAtDetected : public BT::StatefulActionNode
+{
+    public:
+        DriveAtDetected(const std::string& name, const BT::NodeConfiguration& config,
+                        const std::shared_ptr<SimpleManager> ptr,
+                        const int object_label):
+            BT::StatefulActionNode(name, config),
+            node_(ptr),
+            object_label_(object_label) {}
+        
+        static BT::PortsList providedPorts() { return {}; }
+
+        BT::NodeStatus onStart() override   { return this->publish_joy(); }
+        BT::NodeStatus onRunning() override { return this->publish_joy(); }
+
+        void onHalted() override {}
+    
+    private: 
+        std::shared_ptr<SimpleManager> node_;
+        int object_label_;
+
+        std::chrono::steady_clock::time_point timer_;
+        bool not_seen_ = false;
+
+        float target_distance_ = -1.0;
+        float stop_distance_ = 3.0;
+
+        BT::NodeStatus publish_joy();
+
+        sensor_msgs::msg::Joy joy_msg_;
+        nav_msgs::msg::Odometry odometry_msg_;
+
+};
+
+class TurnTowardsBuoy : public BT::StatefulActionNode
+{
+    public:
+        TurnTowardsBuoy(const std::string& name, const BT::NodeConfiguration& config,
+                        const std::shared_ptr<SimpleManager> ptr):
+            BT::StatefulActionNode(name, config),
+            node_(ptr) {
+            this->node_->gate_complete = true;
+            begin_ = std::chrono::steady_clock::now();
+        }
+        
+        static BT::PortsList providedPorts() { return {}; }
+
+        BT::NodeStatus onStart() override   { return this->turn(); }
+        BT::NodeStatus onRunning() override { return this->turn(); }
+
+        void onHalted() override {}
+    
+    private: 
+        std::shared_ptr<SimpleManager> node_;
+
+        std::chrono::steady_clock::time_point begin_;
+        float time_limit_ = 10.0;
 
         BT::NodeStatus turn();
+        nav_msgs::msg::Odometry odometry_msg_;
+
 };
 
 #endif
--- ros2_ws/src/bur_auv/simple_manager/src/bt_nodes.cpp
+++ ros2_ws/src/bur_auv/simple_manager/src/bt_nodes.cpp
@@ -7,14 +7,72 @@
 #include <thread>
 
 #include "tf2/LinearMath/Matrix3x3.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
-#include <tf2/LinearMath/Quaternion.h>
 #include <tf2/utils.h>
 
 #include "sensor_msgs/msg/joy.hpp"
 
-#include "geometry_msgs/msg/quaternion.hpp"
 
+BT::NodeStatus DriveForDuration::publish_joy() {
+    auto end = std::chrono::steady_clock::now();
+    if(std::chrono::duration_cast<std::chrono::seconds> (end - this->begin_).count() > this->duration_) {
+        return BT::NodeStatus::SUCCESS;
+    }
+
+    this->node_->publish_joy_msg(this->joy_msg_);
+
+    return BT::NodeStatus::RUNNING;
+}
+
+
+BT::NodeStatus FireTorpedo::tick() {
+    sensor_msgs::msg::Joy msg;
+
+    auto values = std::vector<double>(9, 0);
+    auto buttons = std::vector<int>(10, 0);
+    
+    values[2] = -0.7;
+    values[0] = 1.0;
+    buttons[9] = 1;
+
+    for (size_t i = 0; i < values.size(); i++) {
+            msg.axes.push_back(values[i]);
+    }
+    for (size_t i = 0; i < buttons.size(); i++) {
+            msg.buttons.push_back(buttons[i]);
+    }
+
+    this->node_->publish_joy_msg(msg);
+
+    return BT::NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus TurnToOrientation::turn() {
+    double roll_state, pitch_state, yaw_state;
+
+    auto current_pos = this->node_->getCurrentPosition();
+    tf2::Quaternion q = tf2::Quaternion(current_pos.orientation.x, current_pos.orientation.y, current_pos.orientation.z, current_pos.orientation.w);
+    tf2::Matrix3x3 rot_matrix = tf2::Matrix3x3(q);
+    rot_matrix.getRPY(roll_state, pitch_state, yaw_state);
+
+    double roll_target, pitch_target, yaw_target;
+    tf2::Quaternion q_target = tf2::Quaternion(target_.x, target_.y, target_.z, target_.w);
+    tf2::Matrix3x3(q_target).getRPY(roll_target, pitch_target, yaw_target);
+
+    if(abs(roll_target - roll_state) < 0.1 && abs(pitch_target - pitch_state) < 0.1
+        && abs(yaw_target - yaw_state) < 0.1) {
+        return BT::NodeStatus::SUCCESS;
+    }
+
+    auto odometry_msg_ = nav_msgs::msg::Odometry();
+    odometry_msg_.pose.pose.orientation = this->target_;
+    odometry_msg_.pose.pose.position.z = 1.0;
+
+    this->node_->publish_odometry_msg(odometry_msg_);
+
+    return BT::NodeStatus::RUNNING;
+}
+
+// Vision Nodes (unused)
 
 int getMatchingDetection(const std::vector<yolo_msgs::msg::CVDetection> detected,
                                     const int label) {
@@ -148,65 +206,4 @@ BT::NodeStatus DriveAtDetected::publish_joy() {
 
         return BT::NodeStatus::RUNNING;
     }
-}
-
-
-BT::NodeStatus DriveForDuration::publish_joy() {
-    auto end = std::chrono::steady_clock::now();
-    if(std::chrono::duration_cast<std::chrono::seconds> (end - this->begin_).count() > this->duration_) {
-        return BT::NodeStatus::SUCCESS;
-    }
-
-    this->node_->publish_joy_msg(this->joy_msg_);
-
-    return BT::NodeStatus::RUNNING;
-}
-
-
-BT::NodeStatus FireTorpedo::tick() {
-    sensor_msgs::msg::Joy msg;
-
-    auto values = std::vector<double>(9, 0);
-    auto buttons = std::vector<int>(10, 0);
-    
-    values[2] = -0.7;
-    values[0] = 1.0;
-    buttons[9] = 1;
-
-    for (size_t i = 0; i < values.size(); i++) {
-            msg.axes.push_back(values[i]);
-    }
-    for (size_t i = 0; i < buttons.size(); i++) {
-            msg.buttons.push_back(buttons[i]);
-    }
-
-    this->node_->publish_joy_msg(msg);
-
-    return BT::NodeStatus::SUCCESS;
-}
-
-BT::NodeStatus TurnToOrientation::turn() {
-    double roll_state, pitch_state, yaw_state;
-
-    auto current_pos = this->node_->getCurrentPosition();
-    tf2::Quaternion q = tf2::Quaternion(current_pos.orientation.x, current_pos.orientation.y, current_pos.orientation.z, current_pos.orientation.w);
-    tf2::Matrix3x3 rot_matrix = tf2::Matrix3x3(q);
-    rot_matrix.getRPY(roll_state, pitch_state, yaw_state);
-
-    double roll_target, pitch_target, yaw_target;
-    tf2::Quaternion q_target = tf2::Quaternion(target_.x, target_.y, target_.z, target_.w);
-    tf2::Matrix3x3(q_target).getRPY(roll_target, pitch_target, yaw_target);
-
-    if(abs(roll_target - roll_state) < 0.1 && abs(pitch_target - pitch_state) < 0.1
-        && abs(yaw_target - yaw_state) < 0.1) {
-        return BT::NodeStatus::SUCCESS;
-    }
-
-    auto odometry_msg_ = nav_msgs::msg::Odometry();
-    odometry_msg_.pose.pose.orientation = this->target_;
-    odometry_msg_.pose.pose.position.z = 1.0;
-
-    this->node_->publish_odometry_msg(odometry_msg_);
-
-    return BT::NodeStatus::RUNNING;
 }
